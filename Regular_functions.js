6c08510d2d3fd0ecd209ec4561cac10e6c04e072
Neema_Mwende <gracepete12@gmail.com>
2024-01-18 15:54:56 +0300

    regular function syntax

---
+++ Regular_functions.js
@@ -0,0 +1,7 @@
+
+//To return a value from a regular function, 
+//you have to explicitly use the return keyword. Otherwise the function will return undefined.
+const name = 'Angel';
+function sayHello(name) {
+    return 'Hello ' + name
+  }
