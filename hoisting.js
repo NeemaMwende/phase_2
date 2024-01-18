740ea6a5852e63841d685ae5b0cb909242361e09
Neema_Mwende <gracepete12@gmail.com>
2024-01-18 16:38:58 +0300

    this keyword

--- hoisting.js
+++ hoisting.js
@@ -12,12 +12,27 @@ function regularFunction() {
 //   console.log("This is an arrow function.")
 // }
 
+//For regular functions, a simple function call sets the this value to the window object (or to undefined if you're using the "strict mode"):
+function myRegularFunction() {
+    console.log(this)
+  }
+      
+  myRegularFunction()
+//or
+  "use strict"
 
-
-
-
-
-
+  function myFunction() {
+    console.log(this)
+  }
+      
+  myFunction() // udefined
+  
+// this in arror functions
+  const myArrowFunction = () => {
+    console.log(this);
+  };
+  
+  myArrowFunction()
 
 
 
