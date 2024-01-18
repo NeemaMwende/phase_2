bb98dbc56343ad5a23e8b2736370b7ae157d7062
Neema_Mwende <gracepete12@gmail.com>
2024-01-18 16:13:55 +0300

    accessing parameters

--- Regular_functions.js
+++ Regular_functions.js
@@ -7,10 +7,26 @@ function sayHello(name) {
   }
 
 //arrow functions
-const sayHello = (name) => {
-    return 'Hello ' + name
-  }
+// const sayHello = (name) => {
+//     return 'Hello ' + name
+//   }
 
-  //or
-  const sayHello = (name) => 'Hello ' + name
-  const sayHello = name => 'Hello ' + name
+//   //or
+//   const sayHello = (name) => 'Hello ' + name
+//   const sayHello = name => 'Hello ' + name
+
+  
+//to access with regular functions = arguments (not available in arrow functions instead = ...args)
+function logNumbers(num1, num2) {
+    console.log(arguments)
+  }
+  
+  logNumbers(8, 24)
+
+  function logNumbers(...args) {
+    console.log(args)
+  }
+  
+  logNumbers(8, 24, 34, 89, 45)
+
+  
