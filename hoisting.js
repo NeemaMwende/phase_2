29d3242dada305d01e28c314ca9896b658e2e6e4
Neema_Mwende <gracepete12@gmail.com>
2024-01-18 16:29:08 +0300

    function hoisting

---
+++ hoisting.js
@@ -0,0 +1,25 @@
+//in regular functions
+regularFunction()
+
+function regularFunction() {
+  console.log("This is a regular function.")
+}
+
+//Hoisting in arrow functions dont work
+// arrowFunction()
+
+// const arrowFunction = () => {
+//   console.log("This is an arrow function.")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
